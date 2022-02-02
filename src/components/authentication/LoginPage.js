255715917a4e6ea0a954b88c5db1abf8ff4d8f15
Emilien <emilien.paris@etudiant.univ-lr.fr>
2022-02-02 12:57:00 +0100

    :bug: : updating login page

--- src/components/authentication/LoginPage.js
+++ src/components/authentication/LoginPage.js
@@ -1,9 +1,11 @@
 import AuthenticationButton from "./AuthenticationButton";
 import { useAuth0 } from "@auth0/auth0-react"
 import { Link } from "react-router-dom";
+import { browserHistory  } from "react-router";
 const LoginPage = (props) => {
     const { user, isAuthenticated } = useAuth0()
     console.log(isAuthenticated)
+    browserHistory.push("/")
     return(
         <div style= {{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <AuthenticationButton />
