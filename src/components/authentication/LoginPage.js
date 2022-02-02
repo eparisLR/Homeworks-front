40d87731929d91606c50cd6cb117a05497c4d503
Emilien <emilien.paris@etudiant.univ-lr.fr>
2022-02-02 13:22:05 +0100

    :bug: : correcting redirect after login

--- src/components/authentication/LoginPage.js
+++ src/components/authentication/LoginPage.js
@@ -1,18 +1,13 @@
 import AuthenticationButton from "./AuthenticationButton";
 import { useAuth0 } from "@auth0/auth0-react"
 import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router';
 
 const LoginPage = (props) => {
     const { user, isAuthenticated } = useAuth0()
-    console.log(isAuthenticated)
-
-    const navigate = useNavigate()
 
     return(
         <div style= {{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <AuthenticationButton />
-            <button onClick={() => {navigate("/")}}>Home</button>
             {isAuthenticated && (
                 <div>
                     <img src={user.picture} alt={user.name}/>
