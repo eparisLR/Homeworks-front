06f5900c6c0449c3c276851c36f75a10725980a3
Emilien <emilien.paris@etudiant.univ-lr.fr>
2022-02-02 11:10:06 +0100

    :bug: : updating login page

--- src/components/authentication/LoginPage.js
+++ src/components/authentication/LoginPage.js
@@ -1,10 +1,16 @@
 import AuthenticationButton from "./AuthenticationButton";
-
+import { useAuth0 } from "@auth0/auth0-react"
 const LoginPage = (props) => {
+    const { user, isAuthenticated, isLoading } = useAuth0()
     return(
-        <div style= {{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <AuthenticationButton />
-        </div>
+        isAuthenticated && (
+            <div style= {{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <img src={user.picture} alt={user.name}/>
+                <p>{user.name}</p>
+                <p>{user.email}</p>
+            <AuthenticationButton />
+            </div>
+        )
     )
 }
 
