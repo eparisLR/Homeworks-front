7fd023dead9c270db7c9cc5d9a7d681c8867755a
Emilien <emilien.paris@etudiant.univ-lr.fr>
2022-02-11 09:27:45 +0100

    :sparkles: : styling login page

--- src/components/authentication/LoginPage.js
+++ src/components/authentication/LoginPage.js
@@ -1,20 +1,29 @@
 import AuthenticationButton from "./AuthenticationButton";
 import { useAuth0 } from "@auth0/auth0-react"
 import { Link } from "react-router-dom";
+import background from '../../assets/background-login.jpg'
 
 const LoginPage = (props) => {
     const { user, isAuthenticated } = useAuth0()
 
     return(
-        <div style= {{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <AuthenticationButton />
-            {isAuthenticated && (
+        <div style= {{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${background})`}}>
+            <div style={{ width: '20vw', height: '20vh', display: 'flex', flexDirection: 'column',justifyContent: 'center', backgroundColor: 'whitesmoke', borderRadius: '5px', padding: '1vw', textAlign: 'center'}}>
+                {!isAuthenticated && (
+                    <div>
+                        <h2>Connectez-vous</h2>
+                        <p>Veuillez vous connectez pour accéder à la liste de vos travaux</p>
+                    </div>
+                )}
+                <AuthenticationButton/>
+                {isAuthenticated && (
                 <div>
                     <img src={user.picture} alt={user.name}/>
                     <p>{user.name}</p>
                     <Link to="/homeworks"> DashBoard </Link>
                 </div>
             )}
+            </div>
         </div>
     )
 }
