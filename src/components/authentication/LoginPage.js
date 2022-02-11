45054ccc60e52a78f47b8a5c412d2688ba71e09f
Emilien <emilien.paris@etudiant.univ-lr.fr>
2022-02-11 09:51:48 +0100

    :sparkles: : styling login page

--- src/components/authentication/LoginPage.js
+++ src/components/authentication/LoginPage.js
@@ -18,8 +18,8 @@ const LoginPage = (props) => {
                     </div>
                 )}
                 {isAuthenticated && (
-                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
-                    <img src={user.picture} alt={user.name} style={{borderRadius: '20px'}}/>
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>
+                    <img src={user.picture} alt={user.name} style={{borderRadius: '20px', width: '10vw', marginBottom: '1vh'}}/>
                     <Link to="/homeworks" style={{ color: 'inherit', textDecoration: 'inherit'}}>
                         <Button variant="contained">Liste de vos travaux</Button>
                     </Link>
