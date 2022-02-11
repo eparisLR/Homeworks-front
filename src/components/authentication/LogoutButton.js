9bac1d7093d0b6c08c955f78ff65e2ec1363e0a7
Emilien <emilien.paris@etudiant.univ-lr.fr>
2022-02-11 10:20:22 +0100

    :sparkles: : adding logout icon

--- src/components/authentication/LogoutButton.js
+++ src/components/authentication/LogoutButton.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from '@mui/material/Button'
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <Button variant="contained" onClick={() => logout({ returnTo: window.location.origin })}>
+    <Button variant="contained" onClick={() => logout({ returnTo: window.location.origin })} startIcon={<LogoutIcon />}>
       Déconnexion
     </Button>
   );
